c75e30d6ff271ddb06efecdb0a5abc7462241f89
John Freeman <john.freeman.opensource@gmail.com>
2018-09-06 13:43:15 +0100

    Changed tests to use host fixture (#79)
    Other fixtures are deprecated and will be removed in 2.0 Testinfra release.

--- molecule/default/tests/test_role.py
+++ molecule/default/tests/test_role.py
@@ -6,10 +6,10 @@ testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
     os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
 
 
-def test_maven_color(Command):
-    assert Command("mvn help:help").rc == 0
+def test_maven_color(host):
+    assert host.run("mvn help:help").rc == 0
 
     # Need to run with bash for ANSI-C Quoting support
-    cmd = Command("bash -c %s",
-                  "mvn help:help -Dmaven.color=true | grep -Eq $'\e'")
+    cmd = host.run("bash -c %s",
+                   "mvn help:help -Dmaven.color=true | grep -Eq $'\e'")
     assert cmd.rc == 0
